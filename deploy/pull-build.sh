1d827761cdffcad2d2df788c522c09f0928299c6
论文知识库初始化 <paper-kb@localhost>
2026-08-06 15:59:34 +0000

    固定 GitHub 部署密钥的主机校验

--- deploy/pull-build.sh
+++ deploy/pull-build.sh
@@ -6,7 +6,7 @@ source_dir="$deploy_root/source"
 site_dir="$deploy_root/site"
 deploy_key="$deploy_root/github-deploy-key"
 
-export GIT_SSH_COMMAND="ssh -i $deploy_key -o IdentitiesOnly=yes -o StrictHostKeyChecking=yes"
+export GIT_SSH_COMMAND="ssh -i $deploy_key -o IdentitiesOnly=yes -o StrictHostKeyChecking=yes -o UserKnownHostsFile=$deploy_root/github-known-hosts"
 
 git -C "$source_dir" fetch --prune origin main
 current=$(git -C "$source_dir" rev-parse HEAD)
@@ -25,4 +25,3 @@ git -C "$source_dir" reset --hard origin/main
 docker compose \
     -f "$source_dir/deploy/oauth/docker-compose.yml" \
     up -d --build --remove-orphans
-
